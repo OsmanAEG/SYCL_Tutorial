96a0baaeeb7f977924ac8599757db6fc7988da73
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-07-18 18:54:24 -0400

    inquiry updated

--- 1_Queue/device_inquiry.h
+++ 1_Queue/device_inquiry.h
@@ -5,15 +5,16 @@ void print_available_devices(){
   int p = 0;
   auto platforms = sycl::platform::get_platforms();
   for(auto& platform : platforms){
-    std::cout << "PLATFORM" << p << ": "
+    std::cout << "PLATFORM " << p << " | "
               << platform.template get_info<sycl::info::platform::name>()
-              << "\n WITH DEVICES: \n"
+              << " | WITH DEVICES:\n"
+              << "---------------------------------------------------------"
               << std::endl;
     int d = 0;
     auto devices = platform.get_devices();
     for(auto& device : devices){
       // printing device information
-      std::cout << "DEVICE" << d << ": "
+      std::cout << " - DEVICE " << d << ": "
                 << device.template get_info<sycl::info::device::name>()
                 << std::endl;
       d++;
